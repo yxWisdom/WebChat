e8b9d5dcd2d0751801e203b6896d1e485475a450
luluZoe <907604372@qq.com>
2017-06-19 13:13:08 +0800

    myTableClass

---
+++ src/my/Info.java
@@ -0,0 +1,42 @@
+package my;
+
+public class Info {
+
+    private String accountid;
+    private String nickname;
+    private String gender;
+    private String birthday;
+    private String photo;
+
+    public String getAccountid() {
+        return accountid;
+    }
+    public void setAccountid(String accountid) {
+        this.accountid = accountid;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getBirthday() {
+        return birthday;
+    }
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+    public String getPhoto() {
+        return photo;
+    }
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+}
